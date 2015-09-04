ff02b0114f888bba43e8ba515a9309736662abde
jeanettearteaga <jay8chuy@gmail.com>
2015-09-03 20:23:44 -0600

    Set up the program

---
+++ PS1/FormulaEvaluator/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FormulaEvaluator
+{
+    public class Class1
+    {
+    }
+
+
+    public static class Evaluator
+    {
+
+        public delegate int Lookup(String v);
+
+        public static int Evaluate(String exp, Lookup variableEvaluator)
+        {
+            Stack<Char> operators = new Stack<char>();
+            Stack<int> operands = new Stack<int>();
+
+            string[] substring = Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+            for (int index = 0; index < substring.Length; index++)
+            {
+
+            }
+            return 0;
+        }
+    }
+}
