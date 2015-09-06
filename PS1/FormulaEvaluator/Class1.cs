a6a6894b53b314406aef604e006e610dc7b1d3f4
jeanettearteaga <jay8chuy@gmail.com>
2015-09-05 18:23:53 -0600

    Added all possible tokens except for variable. Without error checks.

--- PS1/FormulaEvaluator/Class1.cs
+++ PS1/FormulaEvaluator/Class1.cs
@@ -23,18 +23,135 @@ namespace FormulaEvaluator
             Stack<int> Value = new Stack<int>();
 
             string[] token = Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+
             for (int index = 0; index < token.Length; index++)
             {
                 Boolean isDigit = Regex.IsMatch(token[index], @"\d");
-                //if (isDigit && (operators(char).Peek.equals("*") || ))
-                //{
-                //  Value<int>.Push(token[index]);
-                //}
-                //else
-                //{
-                //  operators<Char>.Push(substring[index]);
-                //}
+                Boolean isVariable;
+                Boolean isPlus = Regex.IsMatch(token[index], "+");
+                Boolean isMinus = Regex.IsMatch(token[index], "-");
+                Boolean isMultiplication = Regex.IsMatch(token[index], "*");
+                Boolean isDivide = Regex.IsMatch(token[index], "/");
+                Boolean isLeftParenth = Regex.IsMatch(token[index], "(");
+                Boolean isRightParenth = Regex.IsMatch(token[index], ")");
 
+                // Checks if the curretn token is an integer then prosecutes in 
+                // a certain way depending on what is at the highest position
+                // in the operators stack.
+                if (isDigit)
+                {
+                    int currentToken = Int32.Parse(token[index]);
+
+                    if(!operators.Peek().Equals('*') || !operators.Peek().Equals('/'))
+                    {
+                        Value.Push(currentToken);
+                    }
+
+                    switch (operators.Peek())
+                    {
+                        case '*':
+                            int currentVal = Value.Pop();
+                            operators.Pop();
+                            int newVal = (currentVal * currentToken);
+                            Value.Push(newVal);
+                            break;
+
+                        case '/':
+                            currentVal = Value.Pop();
+                            operators.Pop();
+                            newVal = (currentVal / currentToken);
+                            Value.Push(newVal);
+                            break;
+                    }
+                }
+
+                if (isPlus || isMinus)
+                {
+                    char currentToken = Convert.ToChar(token[index]);
+
+                    switch (operators.Peek())
+                    {
+                        case '+':
+                            int val1 = Value.Pop();
+                            int val2 = Value.Pop();
+                            operators.Pop();
+                            int newVal = val1 + val2;
+                            Value.Push(newVal);
+                            operators.Push(currentToken);
+                            break;
+
+                        case '-':
+                            val1 = Value.Pop();
+                            val2 = Value.Pop();
+                            operators.Pop();
+                            newVal = val1 + val2;
+                            Value.Push(newVal);
+                            operators.Push(currentToken);
+                            break;
+                    }
+                }
+
+                else if (isMultiplication || isDivide)
+                {
+                    char currentToken = Convert.ToChar(token[index]);
+                    operators.Push(currentToken);
+                }
+
+                else if (isLeftParenth)
+                {
+                    char currentToken = Convert.ToChar(token[index]);
+                    operators.Push(currentToken);
+                }
+
+                else if (isRightParenth)
+                {
+                    char currentToken = Convert.ToChar(token[index]);
+
+                    switch (operators.Peek())
+                    {
+                        case '+':
+                            int val1 = Value.Pop();
+                            int val2 = Value.Pop();
+                            operators.Pop();
+                            int newVal = val1 + val2;
+                            Value.Push(newVal);
+                            break;
+
+                        case '-':
+                            val1 = Value.Pop();
+                            val2 = Value.Pop();
+                            operators.Pop();
+                            newVal = val1 - val2;
+                            Value.Push(newVal);
+                            break;
+                    }
+
+                    //if (operators.Peek().Equals('('))
+                    //{
+                    //    operators.Pop();
+                    //}
+                    //else
+                    //    thorw error
+
+                    switch (operators.Peek())
+                    {
+                        case '*':
+                            int val1 = Value.Pop();
+                            int val2 = Value.Pop();
+                            operators.Pop();
+                            int newVal = val1 * val2;
+                            Value.Push(newVal);
+                            break;
+
+                        case '/':
+                            val1 = Value.Pop();
+                            val2 = Value.Pop();
+                            operators.Pop();
+                            newVal = val1 / val2;
+                            Value.Push(newVal);
+                            break;
+                    }
+                }
             }
             return 0;
         }
